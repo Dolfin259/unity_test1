a58d00c84538982e5e8c3010c87c30449a881f0a
KEY <endolfin@outlook.jp>
2023-03-10 22:15:38 +0900

    差分コミット

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -45,6 +45,7 @@ public class Player : MonoBehaviour
         int hp = Mathf.Abs(5);
         ControlLostTime = 0f;
         audiosource = GetComponent<AudioSource>();
+        anim.keepAnimatorControllerStateOnDisable = true;
     }
     
     void Update()
